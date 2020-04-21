3bf0a8dbf546a83f3d7903a698f1e68a06b637ed
Stéphane Menut <smenut@dawan.fr>
2020-04-21 17:17:06 +0200

    routage des composants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from './component/base/app.component';
 import { PresentationComponent } from './component/presentation/presentation.component';
 import { TodoComponent } from './component/todo/todo.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
+import { FiltreComponent } from './component/filtre/filtre.component';
+import { DirectiveComponent } from './component/directive/directive.component';
+import { LocaleComponent } from './component/locale/locale.component';
 
 const routes: Routes = [
   { path: 'home', component: AppComponent },
   { path: 'presentation', component: PresentationComponent },
   { path: 'todo', component: TodoComponent },
+  { path: 'filtre', component: FiltreComponent },
+  { path: 'directive', component: DirectiveComponent },
+  { path: 'locale', component: LocaleComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // prefix
   { path: '**', component: NotFoundComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { registerLocaleData } from '@angular/common';
+import localefr from '@angular/common/locales/fr';
+import localede from '@angular/common/locales/de';
+import localeja from '@angular/common/locales/ja';
+
+registerLocaleData(localefr);
+registerLocaleData(localede);
+registerLocaleData(localeja);
 
 import { AppComponent } from './component/base/app.component';
 import { PresentationComponent } from './component/presentation/presentation.component';
 import { TodoComponent } from './component/todo/todo.component';
 import { RouteComponent } from './component/route/route.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
+import { FiltreComponent } from './component/filtre/filtre.component';
+import { DirectiveComponent } from './component/directive/directive.component';
+import { LocaleComponent } from './component/locale/locale.component';
 
 @NgModule({
   declarations: [
@@ -16,14 +28,20 @@ import { NotFoundComponent } from './component/not-found/not-found.component';
     PresentationComponent,
     TodoComponent,
     RouteComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    FiltreComponent,
+    DirectiveComponent,
+    LocaleComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' }
+  ],
   // bootstrap: [AppComponent, PresentationComponent, TodoComponent]
   bootstrap: [RouteComponent]
 })
